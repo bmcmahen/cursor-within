7cf2f326fbd88990e747ce091745ef42432938a1
bmcmahen <ben.mcmahen@gmail.com>
2014-08-16 01:54:57 -0600

    small changes to tests

--- test/cursor-within.js
+++ test/cursor-within.js
@@ -4,26 +4,25 @@ var range = require('selection-range');
 var within = require('cursor-within');
 
 describe('cursor-within', function () {
-  var $el, el;
 
   beforeEach(function () {
-    $el = dom('<div><p><span>hi there what up eh</span></p></div>').appendTo(document.body);
-    el = $el[0];
+    this.$el = dom('<div><p><span>hi there what up eh</span></p></div>').appendTo(document.body);
+    this.el = this.$el[0];
   });
 
   afterEach(function () {
-    $el.remove();
+    this.$el.remove();
   });
 
   it('should match the first el which matches', function () {
-    range(el, {start: 3});
-    assert(within('p') == $el.find('p')[0]);
-    assert(within('span') == $el.find('span')[0]);
+    range(this.el, {start: 3});
+    assert(within('p') == this.$el.find('p')[0]);
+    assert(within('span') == this.$el.find('span')[0]);
   });
 
   it('should match first el when no match specified', function () {
-    range(el, {start: 3});
-    assert(within() == $el.find('span')[0]);
+    range(this.el, {start: 3});
+    assert(within() == this.$el.find('span')[0]);
   })
 
 });
